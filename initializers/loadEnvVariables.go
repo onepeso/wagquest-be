586507fe9c2e3c38de732f2c1d6accad6959f6a2
onepeso <giovanicode@outlook.com>
2024-04-12 00:25:50 -0700

    better .env loader

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -1,15 +1,35 @@
 package initializers
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
+	"os"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// Fallback values if .env file is not present or loaded
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "default_db_host"
 	}
-}
 
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "default_db_port"
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "default_db_user"
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "default_db_password"
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "default_db_name"
+	}
+
+	// Use dbHost, dbPort, dbUser, dbPassword, dbName, and other environment variables as needed
+}
