4e08ec384f822b7cec2ee73854a71ca98787a2ed
onepeso <giovanicode@outlook.com>
2024-04-12 00:30:24 -0700

    add ssl

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	// Read database connection information from environment variables
-	dsn := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable"
+	dsn := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=require"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
