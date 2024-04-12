07d51ae4229d2d2654b335c13832cf034075bd3b
onepeso <giovanicode@outlook.com>
2024-04-12 00:05:51 -0700

    add correct fields on database to connect

--- initializers/database.go
+++ initializers/database.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +12,8 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "host=localhost port=5433 user=postgres password=postgres dbname=pawadventures-backend sslmode=disable"
+	// Read database connection information from environment variables
+	dsn := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
