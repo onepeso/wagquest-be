7d3f703e19cd8dd132e1669ed31fa9468f7d7f0e
onepeso <giovanicode@outlook.com>
2024-04-24 01:31:38 -0700

    Revert back to live site database config

--- initializers/database.go
+++ initializers/database.go
@@ -1,35 +1,9 @@
 //DEV
 
-package initializers
-
-import (
-	"log"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-var DB *gorm.DB
-
-func ConnectToDB() {
-	var err error
-	dsn := "host=localhost port=5433 user=postgres password=postgres dbname=pawadventures-backend sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
-	}
-	log.Println("Connected to the database!")
-}
-
-
-
-// PROD 
-
 // package initializers
 
 // import (
 // 	"log"
-// 	"os"
 
 // 	"gorm.io/driver/postgres"
 // 	"gorm.io/gorm"
@@ -39,11 +13,35 @@ func ConnectToDB() {
 
 // func ConnectToDB() {
 // 	var err error
-// 	// Read database connection information from environment variables
-// 	dsn := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=require"
+// 	dsn := "host=localhost port=5433 user=postgres password=postgres dbname=pawadventures-backend sslmode=disable"
 // 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 // 	if err != nil {
 // 		log.Fatalf("Error connecting to the database: %v", err)
 // 	}
 // 	log.Println("Connected to the database!")
 // }
+
+// PROD
+
+package initializers
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	// Read database connection information from environment variables
+	dsn := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=require"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	log.Println("Connected to the database!")
+}
